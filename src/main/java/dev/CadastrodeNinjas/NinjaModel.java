d0f035e58d2456e801f9b662c99184c4587dce29
Thalys Grillo <thalyscg@gmail.com>
2025-06-07 23:42:53 -0300

    teste de git

--- src/main/java/dev/CadastrodeNinjas/NinjaModel.java
+++ src/main/java/dev/CadastrodeNinjas/NinjaModel.java
@@ -1,4 +1,51 @@
 package dev.CadastrodeNinjas;
 
-public class Ninjamodel {
+import jakarta.persistence.*;
+
+//Entity transforma uma classe em entidade no DB
+@Entity
+//
+@Table(name ="tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    Integer idade;
+
+    public NinjaModel(){
+
+    }
+
+    public NinjaModel(String nome, String email, Integer idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
