b8f94999391145ebf8e61e4983ea988190ecdd7d
FAT_KRICKET <thalyscg@gmail.com>
2025-06-12 17:07:08 -0300

    lombok

--- src/main/java/dev/CadastrodeSoldados/Missoes/MissoesModel.java
+++ src/main/java/dev/CadastrodeSoldados/Missoes/MissoesModel.java
@@ -1,13 +1,23 @@
 package dev.CadastrodeSoldados.Missoes;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import dev.CadastrodeSoldados.Soldados.SoldadoModel;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nome_missao;
     private String nivel_missao;
     private Long Id_missoes;
+
+
+    // Uma missão pode ter vários soldados
+    @OneToMany(mappedBy = "missoes")
+    private List<SoldadoModel> soldados;
+
 }
--- src/main/java/dev/CadastrodeSoldados/Soldados/SoldadoModel.java
+++ src/main/java/dev/CadastrodeSoldados/Soldados/SoldadoModel.java
@@ -1,11 +1,18 @@
 package dev.CadastrodeSoldados.Soldados;
 
+import dev.CadastrodeSoldados.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe em entidade no DB
 @Entity
-//
+//tabela de cadastro dos soldados
 @Table(name ="tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class SoldadoModel {
 
     @Id
@@ -15,37 +22,10 @@ public class SoldadoModel {
     String email;
     Integer idade;
 
-    public SoldadoModel(){
+    //Um Soldado só poderá realizar uma única missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //Foreign Key
+    private MissoesModel missoes;
 
-    }
 
-    public SoldadoModel(String nome, String email, Integer idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
 }
